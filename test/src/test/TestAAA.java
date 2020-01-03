e6e2490e052c5b59abeb8e0ca629f11f84dfd67f
UID <UID@DESKTOP-DACJOCN>
2020-01-03 11:31:59 +0800

    asasda

--- test/src/test/TestAAA.java
+++ test/src/test/TestAAA.java
@@ -1,13 +1,11 @@
 package test;
 
 public class TestAAA {
-<<<<<<< HEAD
-	int a=10;
+
+		int a=10;
       public static void mian(String[] args){
       int abc = 1111;
-=======
-      public static void mian(String[] args){
+
       
->>>>>>> branch 'master' of https://github.com/czl1990/One.git
       }
 }
