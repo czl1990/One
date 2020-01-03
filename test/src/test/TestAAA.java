772682b445a670c4106ac5eb0fcd6a0dd6a4a5ad
UID <UID@DESKTOP-DACJOCN>
2020-01-03 11:23:37 +0800

    dd

--- test/src/test/TestAAA.java
+++ test/src/test/TestAAA.java
@@ -1,10 +1,5 @@
 package test;
 
 public class TestAAA {
-	public static void main(String[] args) {
-		System.out.println("abc");
-	}
-      public static void mian(String agre){
-      
-      }
+	int a=10;
 }
